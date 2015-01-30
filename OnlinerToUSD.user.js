2c8febf121dd0e1a70d98fe725d1ce82594b6e6b
Maksim_Golunko <maksim_golunko@epam.com>
2015-01-31 01:25:50 +0300

    format

--- OnlinerToUSD.user.js
+++ OnlinerToUSD.user.js
@@ -8,25 +8,26 @@
 // @grant        none
 // ==/UserScript==
 
-(function($) {
-	var curs = +$(".top-informer-currency span._u").text().replace(/[ $]/g, "");
-	function replacePrices(selector) {    
-		$(selector).each(function(){
-			var c = $(this).text();
-			var r = /([\d\s]+)(?:- ([\d\s]+))?/.exec(c);
-			var p1 = +r[1].replace(/\s/g, "");
-			var p1usd = Math.ceil(p1 / curs);
-			var text = "$" + p1usd;
-            var title = r[1];
-			if(r[2]){
-				var p2 = +r[2].replace(/\s/g, "");        
-				var p2usd = Math.ceil(p2 / curs);
-				text += " - $" + p2usd;
-                title += "- " + r[2];
-			}        
-			$(this).text(text).attr("title", title + "руб.");
-		});
-	}
+(function ($) {
+    var curs = +$(".top-informer-currency span._u").text().replace(/[ $]/g, "");
 
-	replacePrices(".pprice");
+    function replacePrices(selector) {
+        $(selector).each(function () {
+            var c = $(this).text();
+            var r = /([\d\s]+)(?:- ([\d\s]+))?/.exec(c);
+            var p1 = +r[1].replace(/\s/g, "");
+            var p1usd = Math.ceil(p1 / curs);
+            var text = "$" + p1usd;
+            var title = r[1];
+            if (r[2]) {
+                var p2 = +r[2].replace(/\s/g, "");
+                var p2usd = Math.ceil(p2 / curs);
+                text += " - $" + p2usd;
+                title += "- " + r[2];
+            }
+            $(this).text(text).attr("title", title + "пїЅпїЅпїЅ.");
+        });
+    }
+
+    replacePrices(".pprice");
 })(jQuery);
